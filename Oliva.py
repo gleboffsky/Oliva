637f9f54bfa778059834ceb6b37313546e365de9
betterty <betterty@mail.ruu>
2023-05-29 23:23:46 +0300

    Add functional func

--- Oliva.py
+++ Oliva.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5 import QtWidgets, QtGui
 import cv2 as cv
 import numpy as np
-
+import os
 
 class Example(QtWidgets.QWidget):
 
@@ -11,6 +11,7 @@ class Example(QtWidgets.QWidget):
         self.initUI()
         self.dirlist_input = ""
         self.path_file_output = ""
+        self.result = ""
 
     def initUI(self):
         self.gridLayout = QtWidgets.QGridLayout(self)
@@ -23,6 +24,7 @@ class Example(QtWidgets.QWidget):
         self.btn2.clicked.connect(self.path_file_output)
         self.btn3 = QtWidgets.QPushButton('Старт', self)
         self.btn3.move(100, 100)
+        self.btn3.clicked.connect(self.functional)
         self.gridLayout.addWidget(self.btn, 0, 0, 1, 1)
         self.gridLayout.addWidget(self.btn2, 0, 2, 1, 1)
         self.gridLayout.addWidget(self.btn3, 1, 1, 1, 1)
@@ -37,29 +39,34 @@ class Example(QtWidgets.QWidget):
         self.dirlist_output = QtWidgets.QFileDialog.getExistingDirectory(None, "Выбрать папку", ".")
 
     def functional(self):
+        try:
+            photos = os.listdir(self.dirlist_input)
+            for photo in photos:
+                img = cv.imread(f"{self.dirlist_input}\\{photo}")
+                gr = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+                bl = cv.medianBlur(gr, 5)
+                canny = cv.Canny(bl, 90, 300)
+                kernel = cv.getStructuringElement(cv.MORPH_RECT, (10, 10))
+                closed = cv.morphologyEx(canny, cv.MORPH_CLOSE, kernel)
+                contours = cv.findContours(closed.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[0]
 
-        img = cv.imread("C:\\Users\\betterty\\Downloads\\Telegram Desktop\\zzzz.jpg")
-        gr = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        bl = cv.medianBlur(gr, 5)
-        canny = cv.Canny(bl, 90, 300)
-        kernel = cv.getStructuringElement(cv.MORPH_RECT, (10, 10))
-        closed = cv.morphologyEx(canny, cv.MORPH_CLOSE, kernel)
-        contours = cv.findContours(closed.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[0]
+                for cnt in contours:
+                    cv.drawContours(img, contours, -1, (255, 0, 0), 2)  # рисуем прямоугольник
+                    M = cv.moments(cnt)
+                    if M["m00"] != 0:
+                        print(M["m00"])
 
-        for cnt in contours:
-            rect = cv.minAreaRect(cnt)  # пытаемся вписать прямоугольник
-            cv.drawContours(img, contours, -1, (255, 0, 0), 2)  # рисуем прямоугольник
-            M = cv.moments(cnt)
-            if M["m00"] != 0:
-                print(M["m00"])
-            cv.putText(img, str(M["m00"]), (np.int0(M["m01"] / M["m00"]), np.int0(M["m10"] / M["m00"])),
-                       cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+                cv.imshow('contours', img)  # вывод обработанного кадра в окно
+                cv.waitKey()
+                cv.destroyAllWindows()
+
+        except Exception as err:
+            print(err)
+            #cv.putText(img, str(M["m00"]), (np.int0(M["m01"] / M["m00"]), np.int0(M["m10"] / M["m00"])), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
 
         # перебираем все найденные контуры в цикле
 
-        cv.imshow('contours', img)  # вывод обработанного кадра в окно
-        cv.waitKey()
-        cv.destroyAllWindows()
+
 
 
 def main():
